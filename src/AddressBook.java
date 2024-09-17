4a294fe7ab339ecd136e2b52ad0bb949fc216841
jakes <jakeswann@cmail.carleton.ca>
2024-09-17 12:24:25 -0400

    COMMIT?

---
+++ src/AddressBook.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<BuddyInfo> buddyList;
+
+    public void addBuddy(BuddyInfo buddy) {
+        if (buddy != null) {
+            buddyList.add(buddy);
+        }
+    }
+
+
+    public void removeBuddy(BuddyInfo buddy) {
+        buddyList.remove(buddy);
+    }
+
+    public static void main(String[] args){
+        System.out.println("Address Book");
+    }
+
+}
